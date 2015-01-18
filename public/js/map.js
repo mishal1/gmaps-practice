88b05ac1d592157ed08f9c2a2a6bb15fe92bb775
Mishal Islam <mishal.islam01@gmail.com>
2015-01-18 18:29:57 +0000

    map centers at users location

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,49 @@
+var map;
+
 function initialize() {
   var mapOptions = {
-    center: { lat: 51.507351, lng: -0.127758},
     zoom: 20
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'You are here!'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    handleNoGeolocation(false);
+  }
 }
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
